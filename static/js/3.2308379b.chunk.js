f350380e116400e499cf78b8ae3a36f0307bed94
elrubiales13 <93713849+elrubiales13@users.noreply.github.com>
2025-06-16 16:49:11 +0200

    Delete static/js/3.2308379b.chunk.js

--- static/js/3.2308379b.chunk.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{37:function(e,o,t){"use strict";e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
-//# sourceMappingURL=3.2308379b.chunk.js.map
